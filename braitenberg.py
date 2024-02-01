b5589a89b7d6ff8a24827f807a325cde4c00c6d6
Ramahatafandry_Maeva <henintsoa.ramahatafandry@etu.sorbonne-universite.fr>
2024-02-01 01:09:29 +0100

    changed hateWall to add front sensors and back sensors

--- braitenberg.py
+++ braitenberg.py
@@ -1,6 +1,10 @@
 def braitenberg_avoider(sensors):
     translation = 1 * sensors["sensor_front"]["distance"]
-    rotation = (-1) * sensors["sensor_front_left"]["distance"] + (1) * sensors["sensor_front_right"]["distance"] 
+    rotation = ((-1) * sensors["sensor_back_left"]["distance"] + (1) * sensors["sensor_back_right"]["distance"] +
+                (-1) * sensors["sensor_front_left"]["distance"] + (1) * sensors["sensor_front_right"]["distance"] +
+                (-1) * sensors["sensor_front"]["distance"] + (1) * sensors["sensor_back"]["distance"] + 
+                (-1) * sensors["sensor_left"]["distance"] + (1) * sensors["sensor_right"]["distance"] 
+             )
     return translation, rotation
 
 def braitenberg_loveWall(sensors):
@@ -10,7 +14,9 @@ def braitenberg_loveWall(sensors):
 
 def braitenberg_hateWall(sensors):
     translation = 1 * sensors["sensor_front"]["distance"]
-    rotation = (-1) * sensors["sensor_front_left"]["distance_to_wall"] + (1) * sensors["sensor_front_right"]["distance"] 
+    rotation = ( (-1) * sensors["sensor_front_left"]["distance_to_wall"] + (1) * sensors["sensor_front_right"]["distance"]  +
+                (-1) * sensors["sensor_front"]["distance_to_wall"] + (1) * sensors["sensor_back"]["distance"] 
+                )
     return translation, rotation
 
 def braitenberg_loveBot(sensors):
