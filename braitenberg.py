578b3c345aa419df71ba7190934a07580aa204d6
batubat <batubat@macbook-air-de-jules.home>
2024-02-08 22:52:25 +0100

    bon braitenberg

--- braitenberg.py
+++ braitenberg.py
@@ -8,23 +8,32 @@ def braitenberg_avoider(sensors):
     return translation, rotation
 
 def braitenberg_loveWall(sensors):
-    translation = 1 * sensors["sensor_front"]["distance_to_wall"]
-    rotation = (1) * sensors["sensor_front_left"]["distance_to_wall"] + (-1) * sensors["sensor_front_right"]["distance"] 
+    translation = 100 * sensors["sensor_front"]["distance"]
+    rotation = ((1) * sensors["sensor_front_left"]["distance_to_wall"] + (-1) * sensors["sensor_front_right"]["distance_to_wall"]+
+                (1) * sensors["sensor_left"]["distance_to_wall"] + (-1) * sensors["sensor_right"]["distance_to_wall"]+
+                (1) * sensors["sensor_back_left"]["distance_to_wall"] + (-1) * sensors["sensor_back_right"]["distance_to_wall"]
+                )
     return translation, rotation
 
 def braitenberg_hateWall(sensors):
     translation = 1 * sensors["sensor_front"]["distance"]
-    rotation = ( (-1) * sensors["sensor_front_left"]["distance_to_wall"] + (1) * sensors["sensor_front_right"]["distance"]  +
+    rotation = ((-1) * sensors["sensor_front_left"]["distance_to_wall"] + (1) * sensors["sensor_front_right"]["distance"]  +
                 (-1) * sensors["sensor_front"]["distance_to_wall"] + (1) * sensors["sensor_back"]["distance"] 
                 )
     return translation, rotation
 
 def braitenberg_loveBot(sensors):
-    translation = 1 * sensors["sensor_front"]["distance_to_robot"]
-    rotation = (1) * sensors["sensor_front_left"]["distance_to_robot"] + (-1) * sensors["sensor_front_right"]["distance"] 
+    translation = (100) * sensors["sensor_front"]["distance"]
+    rotation = ((1) * sensors["sensor_front_left"]["distance_to_robot"] + (-1) * sensors["sensor_front_right"]["distance_to_robot"]+
+                (1) * sensors["sensor_left"]["distance_to_robot"] + (-1) * sensors["sensor_right"]["distance_to_robot"]+
+                (1) * sensors["sensor_back_left"]["distance_to_robot"] + (-1) * sensors["sensor_back_right"]["distance_to_robot"]
+                )
     return translation, rotation
 
 def braitenberg_hateBot(sensors):
     translation = 1 * sensors["sensor_front"]["distance"]
-    rotation = (-1) * sensors["sensor_front_left"]["distance_to_robot"] + (1) * sensors["sensor_front_right"]["distance"] 
+    rotation = ((-1) * sensors["sensor_front_left"]["distance_to_robot"] + (1) * sensors["sensor_front_right"]["distance_to_robot"]+
+                (-1) * sensors["sensor_left"]["distance_to_robot"] + (1) * sensors["sensor_right"]["distance_to_robot"]+
+                (-1) * sensors["sensor_back_left"]["distance_to_robot"] + (1) * sensors["sensor_back_right"]["distance_to_robot"]
+                )
     return translation, rotation
